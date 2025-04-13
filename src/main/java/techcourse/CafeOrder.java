236d5aa0d39d301e0d1cbe2f90be253dcda37472
ChoiDongJoon <adong0808@naver.com>
2025-04-13 23:52:15 +0900

    feat: calculateTotalPrice 메서드 수정

--- src/main/java/techcourse/CafeOrder.java
+++ src/main/java/techcourse/CafeOrder.java
@@ -1,44 +1,25 @@
 package techcourse;
 
-public class CafeOrder {
-    public static int calculateTotalPrice(String[] items, int[] quantities) {
-        int total = 0;
-        for (int i = 0; i < items.length; i++) {
-            int price = 0;
-            if (items[i].equals("아메리카노")) {
-                price = 1200;
-            } else if (items[i].equals("라떼")) {
-                price = 2000;
-            } else if (items[i].equals("모카")) {
-                price = 2500;
-            } else if (items[i].equals("크로와상")) {
-                price = 3000;
-            }
-            total += price * quantities[i];
-        }
+import java.util.List;
 
-        int drinkCount = 0;
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].equals("크로와상")) {
-                drinkCount += quantities[i];
-            }
-        }
+public class CafeOrder {
+
+    public OrderMenus getOrderMenus(final String[] items, final int[] quantities) {
+        return new OrderMenus(items, quantities);
+    }
+
+    public int computeDrinkCount(final OrderMenus orderMenus) {
+        return orderMenus.computeDrinkCount();
+    }
+
+    public int calculateTotalPrice(String[] items, int[] quantities) {
+        OrderMenus orderMenus = getOrderMenus(items, quantities);
+        int total = orderMenus.computeTotalPrice();
+
+        int drinkCount = orderMenus.computeDrinkCount();
 
         if (drinkCount >= 5) {
-            int drinkTotal = 0;
-            for (int i = 0; i < items.length; i++) {
-                if (!items[i].equals("크로와상")) {
-                    int drinkPrice = 0;
-                    if (items[i].equals("아메리카노")) {
-                        drinkPrice = 1200;
-                    } else if (items[i].equals("라떼")) {
-                        drinkPrice = 2000;
-                    } else if (items[i].equals("모카")) {
-                        drinkPrice = 2500;
-                    }
-                    drinkTotal += drinkPrice * quantities[i];
-                }
-            }
+            int drinkTotal = orderMenus.computeDrinkTotalPrice();
             total -= drinkTotal / 10;
         }
 
